7ebe5dc8fa4df51625d967cf053c360a8cf1b99e
andre vieira <andrevieira30@gmail.com>
2026-05-21 13:18:31 -0300

    ajuste dsn oracle render

--- db.py
+++ db.py
@@ -2,7 +2,8 @@ import os
 import oracledb
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 ORACLE_USER = os.getenv("ORACLE_USER")
 ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
@@ -17,9 +18,9 @@ def conectar_oracle():
             dsn=ORACLE_DSN
         )
 
-        print("Conectado ao Oracle com sucesso.")
+        print("Conectado no Oracle com sucesso")
         return conexao
 
-    except Exception as erro:
+    except oracledb.Error as erro:
         print("Erro ao conectar no Oracle:", erro)
         return None
